522ae530a1e03d216be1cd6ebd11cc442e806b91
mitsuri91 <launaymaxime5@gmail.com>
2024-12-11 18:05:32 +0100

    validation exo props

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import "./App.css";
 import PokemonCard from "./components/PokemonCard";
+
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
 function App() {
-	return (
-		<div>
-			<PokemonCard />
-		</div>
-	);
+  return (
+    <div>
+      <PokemonCard pokemon={pokemonList[0]} />
+    </div>
+  );
 }
 
 export default App;
